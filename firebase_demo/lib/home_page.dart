b5ee05a44fced1c5847a2fc7e99bc680fc583657
eso14 <eliana.oden14@gmail.com>
2024-10-03 22:58:17 -0500

    Reading messages from firestore in app

--- firebase_demo/lib/home_page.dart
+++ firebase_demo/lib/home_page.dart
@@ -57,6 +57,7 @@ class HomePage extends StatelessWidget {
                   GuestBook(
                     addMessage: (message) =>
                         appState.addMessageToGuestBook(message),
+                        messages: appState.guestBookMessages, 
                   ),
                 ],
               ],
